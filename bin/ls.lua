a5c5b19dcf96c7a5857d9d1a4f896bff0eaa5741
Ocawesome101 <ocawesome101@gmail.com>
2020-10-18 12:39:42 -0400

    ls is *very* basic now, and add argutil lib (may rename later)

--- bin/ls.lua
+++ bin/ls.lua
@@ -1,8 +1,23 @@
 -- ls --
 
 local fs = require("filesystem")
-local shutil = require("shutil")
+local argutil = require("args")
 
-local args, opts = shutil.parse(...)
+local args, opts = argutil.parse(...)
 
+if opts.help then
+  print([[
+Usage: ls [OPTION]... [FILE]...
+List information about FILEs (the current directory by default).
+Sort options alphabetically.
+]])
+end
+
+local dir
 local pwd = os.getenv("PWD")
+if #args == 0 then dir = pwd end
+
+local files = fs.list(dir)
+table.sort(files)
+for i=1, #files, 1 do io.write(files[i].."\t") end
+print("")
